0ef8a09e0488d6a447df3596058a7bda0ef07658
nataliakartasheva <60737186+nataliakartasheva@users.noreply.github.com>
2020-02-10 02:48:05 -0800

    Create ui.R

---
+++ inst/shiny/sampleGUI/ui.R
@@ -0,0 +1,26 @@
+ui <-
+
+  shiny::fluidPage(shinythemes::shinytheme("cerulean"),
+                shiny::sidebarLayout(
+                  shiny::sidebarPanel(width = 4,
+                               shiny::selectInput(
+                                 inputId = "City",
+                                 label = "Please select City",
+                                 choices = city,
+                                 multiple = TRUE  ),
+                               ),
+
+                  shiny::mainPanel(shiny::tabsetPanel(type="tabs",
+                                        shiny::tabPanel("Comparison Graph",  DT::dataTableOutput(outputId = "mtble2"),
+                                                 shiny::plotOutput("costliving")),
+
+                                        shiny::tabPanel("Data",  DT::dataTableOutput(outputId = "mtble"))
+
+
+
+                  )
+                  )))
+
+
+
+
